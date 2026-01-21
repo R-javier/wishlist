351be2cad8b7c47be5ffe2dc3e633f19da937f61
JLeizza-K <jleizza@kopiustech.com>
2026-01-21 12:41:38 +0000

    fixed axios error

---
+++ src/products/products.service.ts
@@ -0,0 +1,30 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { ProductDTO } from 'src/dto/product.dto';
+import { firstValueFrom } from 'rxjs';
+import { CatalogResponse } from 'src/dto/catalog-response.interface';
+import { HttpService } from '@nestjs/axios';
+
+@Injectable()
+export class ProductsService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  private readonly productsServiceUrl =
+    process.env.CATALOG_SERVICE_URL || 'http://localhost:3001/products';
+  constructor(private readonly httpService: HttpService) {}
+
+  async getProducts(): Promise<ProductDTO[]> {
+    try {
+      const response = await firstValueFrom(
+        this.httpService.get<CatalogResponse>(this.productsServiceUrl),
+      );
+      return response?.data.products ?? [];
+    } catch (error) {
+      throw new HttpException(
+        'Error al obtener productos del servicio externo',
+        HttpStatus.SERVICE_UNAVAILABLE,
+      );
+    }
+  }
+}
