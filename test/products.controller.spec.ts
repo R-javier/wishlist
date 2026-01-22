d3c1257227185ad55c69ca02bac5120b7e26163b
Javier Rodriguez <javiplus.-@live.com.ar>
2026-01-22 12:31:08 -0300

    fix handle errors in products service and controller

--- test/products.controller.spec.ts
+++ test/products.controller.spec.ts
@@ -12,6 +12,8 @@ import { ProductsService } from '../src/products/products.service';
 import { HttpService } from '@nestjs/axios';
 import { of, throwError } from 'rxjs';
 import { AxiosError, AxiosResponse } from 'axios';
+import  {ProductDTO} from './../src/dto/product.dto'
+import {ErrorHandlerService} from './../src/error-handler/error-handler.service'
 
 describe('ProductsController', () => {
   let controller: ProductsController;
@@ -23,7 +25,8 @@ describe('ProductsController', () => {
       controllers: [ProductsController],
       providers: [
         ProductsService,
-        { provide: HttpService, useValue: { get: jest.fn() } },
+        ErrorHandlerService,
+        { provide: HttpService, useValue: { get: jest.fn() } }
       ],
     }).compile();
 
@@ -49,12 +52,7 @@ describe('ProductsController', () => {
       .spyOn(httpService, 'get')
       .mockReturnValue(throwError(() => axiosError));
 
-    try {
-      await controller.getProducts();
-    } catch (error) {
-      expect(error).toBeInstanceOf(BadRequestException);
-      expect(error.message).toBe('Solicitud inválida al servicio de catálogo');
-    }
+      await expect(controller.getProducts()).rejects.toThrow(new BadRequestException('Solicitud inválida al servicio de catálogo'))
   });
 
   it('should return the service default message for 404', async () => {
@@ -70,14 +68,8 @@ describe('ProductsController', () => {
       .spyOn(httpService, 'get')
       .mockReturnValue(throwError(() => axiosError));
 
-    try {
-      await controller.getProducts();
-    } catch (error) {
-      expect(error).toBeInstanceOf(NotFoundException);
-      expect(error.message).toBe(
-        'No se encontraron productos en el servicio de catálogo',
-      );
-    }
+await expect(controller.getProducts()).rejects.toThrow(new NotFoundException('No se encontraron productos en el servicio de catálogo'))
+
   });
   it('should return the service default message for 408', async () => {
     const axiosError = {
@@ -92,15 +84,10 @@ describe('ProductsController', () => {
       .spyOn(httpService, 'get')
       .mockReturnValue(throwError(() => axiosError));
 
-    try {
-      await controller.getProducts();
-    } catch (error) {
-      expect(error).toBeInstanceOf(RequestTimeoutException);
-      expect(error.message).toBe(
-        'El servicio de catálogo tardó demasiado en responder',
-      );
-    }
-  });
+
+      await expect(controller.getProducts()).rejects.toThrow(new RequestTimeoutException('El servicio de catálogo tardó demasiado en responder'))
+
+    });
   it('should return the service default message for 504', async () => {
     const axiosError = {
       response: {
@@ -114,14 +101,8 @@ describe('ProductsController', () => {
       .spyOn(httpService, 'get')
       .mockReturnValue(throwError(() => axiosError));
 
-    try {
-      await controller.getProducts();
-    } catch (error) {
-      expect(error).toBeInstanceOf(ServiceUnavailableException);
-      expect(error.message).toBe(
-        'El servicio de catálogo no está disponible en este momento',
-      );
-    }
+            await expect(controller.getProducts()).rejects.toThrow(new ServiceUnavailableException('El servicio de catálogo no está disponible en este momento'))
+
   });
   it('should return the service default message unhandled errors', async () => {
     const axiosError = {
@@ -136,11 +117,20 @@ describe('ProductsController', () => {
       .spyOn(httpService, 'get')
       .mockReturnValue(throwError(() => axiosError));
 
-    try {
-      await controller.getProducts();
-    } catch (error) {
-      expect(error).toBeInstanceOf(HttpException);
-      expect(error.message).toBe('Error inesperado del servicio de catálogo');
-    }
+            await expect(controller.getProducts()).rejects.toThrow(new HttpException('Error inesperado del servicio de catálogo',
+              500,))
+
   });
+  it('should return a list of products', async () =>{
+    const realHttpService = new HttpService();
+  
+  Object.defineProperty(service, 'httpService', {
+    value: realHttpService,
+    writable: true,
+  });
+
+  const products = await controller.getProducts();
+    expect(products[0]).toBeInstanceOf(ProductDTO)
+
+  })
 });
