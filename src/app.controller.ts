39db3d4db4f164ac1ef30d0c271602efd430cb74
JLeizza-K <jleizza@kopiustech.com>
2026-01-23 13:11:50 +0000

    fixed get and getbyId from products, deleted unnecessary app service and controller, fixed dockerfile

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';      
-import type { Product } from './app.service'
+// import { Controller, Get } from '@nestjs/common';
+// import { AppService } from './app.service';      
+// import type { Product } from './app.service'
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getProduct(): Product {
-    return this.appService.getProduct();
-  }
-}
+//   @Get()
+//   getProduct(): Product {
+//     return this.appService.getProduct();
+//   }
+// }
