2d23a6486a96a9ce7b47b3b4ec302e3c8451dfb6
Javier Rodriguez <javiplus.-@live.com.ar>
2026-01-29 14:46:02 -0300

    add e2e test for products

---
+++ test/products.e2e.spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import  request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+describe('Products (e2e)', () => {
+    let app: INestApplication;
+
+    beforeAll(async() => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+
+    afterAll(async() => {
+        await app.close();
+    });
+
+    it('GET /products - respond  200', async () => {
+        const res = await request(app.getHttpServer())
+        .get('/products');
+        expect(res.status).toBe(200);
+        expect(res.body).toBeDefined();
+    })
+
+    it('GET /products/:id - respond 200', async () => {
+        const res = await request(app.getHttpServer())
+        .get('/products/1');
+        expect(res.status).toBe(200);
+        expect(res.body).toBeDefined();
+    });
+});
